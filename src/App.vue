f0ca88a2adda0bedb2c731c06a427627d71e40f7
Barbarubia <dnl.desimone@gmail.com>
2022-03-22 16:30:23 +0100

    creazione header

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,6 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 @import "./assets/scss/style.scss";
 </style>
--- src/components/HeaderDcComics.vue
+++ src/components/HeaderDcComics.vue
@@ -1,13 +1,87 @@
 <template>
-  <header></header>
+  <header>
+      <div class="logo">
+          <a href="#">
+              <img src="../assets/img/dc-logo.png" alt="Logo DC Comics">
+          </a>
+      </div>
+      <nav class="navbar">
+          <ul>
+            <li v-for="(link, index) in links" :key="index"><a href="link.href">{{ link.text }}</a></li>
+          </ul>
+      </nav>
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'HeaderDcComics'
+  name: 'HeaderDcComics',
+  data () {
+    return {
+      links: [
+        {
+          href: '#',
+          text: 'Characters'
+        },
+        {
+          href: '#',
+          text: 'Comics'
+        },
+        {
+          href: '#',
+          text: 'Movies'
+        },
+        {
+          href: '#',
+          text: 'TV'
+        },
+        {
+          href: '#',
+          text: 'Games'
+        },
+        {
+          href: '#',
+          text: 'Collectibles'
+        },
+        {
+          href: '#',
+          text: 'Videos'
+        },
+        {
+          href: '#',
+          text: 'Fans'
+        },
+        {
+          href: '#',
+          text: 'News'
+        },
+        {
+          href: '#',
+          text: 'Shop'
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
+<style scoper lang="scss">
+header {
+    display: flex;
+    justify-content: space-between;
+    .logo {
+        vertical-align: middle;
+    }
+}
 
+li {
+    display: inline-block;
+    padding: 1rem;
+    text-transform: uppercase;
+    a {
+        display: inline-block;
+        text-decoration: none;
+        color: black;
+    }
+}
 </style>
