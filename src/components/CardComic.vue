e2775fbffcc21536036212c4abad9c4febb468ce
Barbarubia <dnl.desimone@gmail.com>
2022-03-23 16:17:30 +0100

    creato elemento per cards dei comics (manca stile)

---
+++ src/components/CardComic.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="cardImage" :alt="cardTitle">
+    <h2>{{ cardTitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComic',
+  props: {
+    cardImage: String,
+    cardTitle: String
+  }
+}
+</script>
+
+<style>
+
+</style>
